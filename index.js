52ffcfadad74f2e9a1e53fa1a459010cda6b5e81
osmium-aep <145663627+osmium-aep@users.noreply.github.com>
2023-11-21 10:46:23 +0000

    Added scoreboard at the end

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const prompt = require('prompt-sync')();
 
 let randomNumber = Math.floor((Math.random() * 100) +1);
 let guess;
+let chances = 0;
+
+
 do{
 guess = prompt("Guess a number between 0 and 100>_ ");
   if(guess > randomNumber){
@@ -10,5 +13,7 @@ console.log("Your Guess is Too high. Try Again");
     console.log("Your Guess is Too low. Try Again");
   }else if(guess == randomNumber){
     console.log("You guessed it right! Good Job!");
+    chances++;
   }
 }while(guess != randomNumber);
+console.log(`You took ${chances} chances to guess the number`);
